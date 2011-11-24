3be2876ee8b8385232452e76bb78cb2f20cf6864
harrywu304@gmail.com <harrywu304@gmail.com@7b7aee06-4c18-faa0-dff0-4bf95f194e56>
2011-11-24 03:43:42 +0000

    update test case and add one more sample image

--- trunk/test/org/shaitu/easyphoto/util/ImageUtilTest.java
+++ trunk/test/org/shaitu/easyphoto/util/ImageUtilTest.java
@@ -1,7 +1,5 @@
 package org.shaitu.easyphoto.util;
 
-import static org.junit.Assert.fail;
-
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -14,14 +12,19 @@ public class ImageUtilTest {
 
 	@Test
 	public void testCreateNewImage() throws IOException {
-		long t1 = System.currentTimeMillis();
-		BufferedImage srcImg = ImageIO.read(new File("sample/sample01.jpg"));
-		BufferedImage outImg = ImageUtil.createNewImage(srcImg, 512);
-		long t2 = System.currentTimeMillis();
-		System.out.println("crop image cost time:"+(t2-t1)+"ms");
-		ImageUtil.storeImage(outImg, new File("sample/sample01_crop.jpg"), 0.85f);
-		long t3 = System.currentTimeMillis();
-		System.out.println("crop&save image cost time:"+(t3-t1)+"ms");
+		String[] images = { "sample/sample01.jpg", "sample/sample02.jpg" };
+		for (String imgFile : images) {
+			long t1 = System.currentTimeMillis();
+			BufferedImage srcImg = ImageIO
+					.read(new File(imgFile));
+			BufferedImage outImg = ImageUtil.createNewImage(srcImg, 512);
+			long t2 = System.currentTimeMillis();
+			System.out.println("crop image [" + imgFile + "] cost time:" + (t2 - t1) + "ms");
+			ImageUtil.storeImage(outImg, new File(imgFile+".crop.jpg"),
+					0.85f);
+			long t3 = System.currentTimeMillis();
+		}
+
 	}
 
 }
